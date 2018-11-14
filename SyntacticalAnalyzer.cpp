24226d0bd327f708dc8f6e9d14993ec8c944f2bc
ssubrandon <brandon1380.bw@gmail.com>
2018-11-14 15:45:06 -0800

    added functions to syn.cpp

--- SyntacticalAnalyzer.cpp
+++ SyntacticalAnalyzer.cpp
@@ -16,3 +16,59 @@ SyntacticalAnalyzer::~SyntacticalAnalyzer ()
 {
 	delete lex;
 }
+
+int SyntacticalAnalyzer::program(){
+
+}
+
+int SyntacticalAnalyzer::more_defines(){
+
+}			
+
+int SyntacticalAnalyzer::define(){
+
+}
+
+int SyntacticalAnalyzer::stmt_list(){
+
+}
+				
+int SyntacticalAnalyzer::stmt(){
+
+}
+							
+int SyntacticalAnalyzer::literal(){
+
+}
+						
+int SyntacticalAnalyzer::more_tokens(){
+
+}
+					
+int SyntacticalAnalyzer::quoted_lit(){
+
+}			
+					
+int SyntacticalAnalyzer::param_list(){
+
+}
+					
+int SyntacticalAnalyzer::else_part(){
+
+}
+					
+int SyntacticalAnalyzer::stmt_pair(){
+
+}
+					
+int SyntacticalAnalyzer::stmt_pair_body(){
+
+}
+					
+int SyntacticalAnalyzer::action(){
+
+}
+						
+int SyntacticalAnalyzer::any_other_token(){
+
+}
