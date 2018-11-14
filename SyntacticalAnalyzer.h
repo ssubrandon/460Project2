378ee40172a64718490c55751a5dd52b6298e7b7
Caleb C Yost <cyost@blue.cs.sonoma.edu>
2018-11-14 15:33:44 -0800

    added function syn.h

--- SyntacticalAnalyzer.h
+++ SyntacticalAnalyzer.h
@@ -12,6 +12,20 @@ class SyntacticalAnalyzer
     public:
 	SyntacticalAnalyzer (char * filename);
 	~SyntacticalAnalyzer ();
+	int program();
+        int more_defines();
+        int define();
+        int stmt_list();
+        int stmt();
+        int literal();
+        int more_tokens();
+        int quoted_lit();
+        int param_list();
+        int else_part();
+        int stmt_pair();
+        int stmt_pair_body();
+        int action();
+        int any_other_token();
     private:
 	LexicalAnalyzer * lex;
 	token_type token;
